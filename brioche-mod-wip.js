c04dbe988aae896fc33cccb7b283a148c65446cf
CharsonBurensen <1peruejos@Hdsb.ca>
2025-02-03 11:47:15 -0500

    Update brioche-mod-wip.js

--- brioche-mod-wip.js
+++ brioche-mod-wip.js
@@ -2,10 +2,21 @@ elements.syrup = {
     color: "#a13d08",
     behavior: behaviors.LIQUID,
     category: "liquids",
-    viscosity: 9999999,
+    viscosity: 100000,
     state: "liquid",
     density: 720,
 	reactions: {
-		"humans": { elem1: "null", elem2: "humans" },
+		"humans": { elem1: null, elem2: "humans" },
 	}
 };
+
+elements.brioche = {
+    color: "#c2440e",
+    behavior: behaviors.POWDER,
+    category: "food",
+	reactions: {
+		"humans": { elem1: null, elem2: "humans" },
+	}
+};
+
+elements.melted_butter.reactions.bread = { "elem1": null, "elem2":"brioche" }
