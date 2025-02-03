e570f1db4188e057166433fdc9bd1081bda9dda7
CharsonBurensen <1peruejos@Hdsb.ca>
2025-02-03 17:32:52 -0500

    Update brioche.js

--- brioche.js
+++ brioche.js
@@ -60,8 +60,14 @@ elements.fancy_flour = {
     temp: 20,
     tempHigh: 70,
     stateHigh: ["fragrance", "smoke"],
-
 };
+
+//thanks to the creator of stickyslime.js for this code
+elements.syrup.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
 
 elements.melted_butter.reactions.bread = { "elem1": null, "elem2":"brioche" },
 elements.fancy_flour.reactions.water = { "elem1": null, "elem2":"fancy_dough" },
