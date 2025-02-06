35df93bac42d20aaa2279e32157c9fa49eabbe62
CharsonBurensen <1peruejos@Hdsb.ca>
2025-02-06 10:57:43 -0500

    Create stupidity.js

---
+++ stupidity/stupidity.js
@@ -0,0 +1,16 @@
+elements.stupid = {
+    color: ["#eb3486", "#0affda", "#c7fa89"],
+    behavior: behaviors.POWDER,
+    category: "weapons",
+    viscosity: 10000,
+    state: "solid",
+    density: 100,
+    desc: "stupid",
+    temp: 20,
+    tempLow: 19,
+    tempHigh: 21,
+	  reactions: {
+        "water": { elem1:"blaster", elem2:"nuke" },
+        "stupid": { elem1:"cloner", elem2:"nuke" },
+    }
+};
