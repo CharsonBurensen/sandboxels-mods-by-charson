434835edd44a57e9a3cda4a6075fa61b35dd75c8
CharsonBurensen <1peruejos@Hdsb.ca>
2025-02-06 13:21:21 -0500

    Create laitderable.js

---
+++ laitderable.js
@@ -0,0 +1,56 @@
+elements.maple_syrup = {
+    color: "#a13d08",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 100000,
+    state: "liquid",
+    density: 720,
+    isFood: true,
+    desc: "english for sirop derable",
+	  reactions: {
+        "head": { elem1: null, elem2:"head" },
+    }
+};
+
+elements.radioactive_maple_seed = {
+    color: "[#52d156, #5d875f]",
+    behavior: behaviors.POWDER,
+    category: "life",
+    viscosity: 100000,
+    state: "solid",
+    isFood: true,
+    desc: "how does this work",
+  	reactions: {
+        "dirt": { elem1:"maple_nut", elem2:"dirt" },
+    }
+};
+
+elements.maple_nut = {
+    color: "#735e3b",
+    behavior: behaviors.POWDER,
+    category: "life",
+    viscosity: 100000,
+    state: "solid",
+    isFood: true,
+    desc: "I dont think this is biologically accurate",
+    breakInto: ["maple_syrup", "maple_powder"],
+  	reactions: {
+        "dirt": { elem1:"maple_plant", elem2:"maple_plant" },
+    }
+};
+
+elements.maple_plant = {
+    color: "#735e3b",
+    behavior: behaviors.solid,
+    category: "life",
+    viscosity: 100000,
+    state: "solid",
+    isFood: true,
+    desc: "I dont think this is biologically accurate",
+	reactions: {
+        "dirt": { elem1:"maple_plant", elem2:"maple_nut" },
+        "plant": { elem1:"maple_plant", elem2:"maple_plant" },
+        "water": { elem1:"maple_plant", elem2:"maple_plant" },
+    }
+};
+
