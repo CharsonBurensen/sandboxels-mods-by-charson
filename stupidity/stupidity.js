f8452da392fa9893574706884a557418bc1d9afb
CharsonBurensen <1peruejos@Hdsb.ca>
2025-02-06 11:20:32 -0500

    Update stupidity.js

--- stupidity/stupidity.js
+++ stupidity/stupidity.js
@@ -33,3 +33,42 @@ elements.monkey = {
 	"water": { elem1:"earthquake", elem2:"acid" },
     }
 };
+
+elements.almond = {
+    color: ["#855b34", "#9c836b", "#f5e7da"],
+    behavior: behaviors.POWDER,
+    category: "weapons",
+    state: "solid",
+    density: 100,
+    desc: "stupid",
+    temp: 20,
+    tempLow: 10,
+    tempHigh: 40,
+    stateHigh: "nut_sauce",
+    stateLow: "cloner",
+    conduct: 1,
+	  reactions: {
+        "head": { elem1: null, elem2:"head" },
+        "juice": { elem1:"party_popper", elem2:"party_popper" },
+	"water": { elem1:"nut_sauce", elem2:"almond_water" },
+    }
+};
+
+elements.nut_sauce = {
+    color: ["#f2cba7", "#f5e7da"],
+    behavior: behaviors.LIQUID,
+    category: "food",
+    viscosity: 10000,
+    state: "liquid",
+    density: 100,
+    desc: "stupid",
+    temp: 20,
+    tempLow: 0,
+    tempHigh: 50,
+    stateHigh: "incinerate",
+    stateLow: "glue",
+    conduct: 1,
+	  reactions: {
+        "head": { elem1: null, elem2:"head" },
+    }
+};
