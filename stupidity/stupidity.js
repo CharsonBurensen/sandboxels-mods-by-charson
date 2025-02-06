dbd5d6294472db26040c367cd8ebbd637b124185
CharsonBurensen <1peruejos@Hdsb.ca>
2025-02-06 11:08:25 -0500

    Update stupidity.js

--- stupidity/stupidity.js
+++ stupidity/stupidity.js
@@ -15,3 +15,21 @@ elements.stupid = {
 	"dirt": { elem1:"tornado", elem2:"stupid" },
     }
 };
+
+elements.monkey = {
+    color: ["#eb3486", "#0affda", "#c7fa89"],
+    behavior: behaviors.POWDER,
+    category: "weapons",
+    viscosity: 10000,
+    state: "solid",
+    density: 100,
+    desc: "stupid",
+    temp: 20,
+    tempLow: 19,
+    tempHigh: 21,
+	  reactions: {
+        "monkey": { elem1:"monkey", elem2:"tsunami" },
+        "stupid": { elem1:"party_popper", elem2:"cloner" },
+	"water": { elem1:"earthquake", elem2:"acid" },
+    }
+};
