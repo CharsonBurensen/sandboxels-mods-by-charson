c066e4cf6cfd5d65e9fa42f25c7537591abf504c
CharsonBurensen <1peruejos@Hdsb.ca>
2025-02-20 08:20:36 -0500

    Update thiquovite.js

--- thiquovite.js
+++ thiquovite.js
@@ -14,9 +14,14 @@ elements.thiquovite = {
 };
 
 elements.screen = {
-    color: "#505e4c",
+    color: ["#505e4c", "#4b6943"],
     behavior: behaviors.WALL,
-    category: "special",
+    behaviorOn: [
+    "SW:screen|XX|SW:screen",
+    "XX|XX|XX",
+    "SW:screen|XX|SW:screen",
+],
+    category: "machines",
     state: "solid",
     conduct: 1,
     breakInto: ["glass_shard", "electric"],
@@ -32,3 +37,34 @@ elements.ring = {
     breakInto: ["silver", "diamond"],
 };
 
+elements.pen = {
+    color: "#454545",
+    behavior: [
+    "XX|XX|XX",
+    "XX|XX|XX",
+    "XX|CR:ink,blue_ink,red_ink|XX",
+],
+    category: "machines",
+    state: "solid",
+    density: 100,
+    conduct: 1,
+    breakInto: ["ink", "metal_scrap"],
+};
+
+elements.blue_ink = {
+    color: "#454545",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    stain: 0.3,
+    breakInto: ["ink", "copper_sulfate"],
+};
+
+elements.red_ink = {
+    color: "#454545",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    stain: 0.3,
+    breakInto: ["ink", "copper_sulfate"],
+};
