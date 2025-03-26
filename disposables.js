0aa2844d5d4bd27c9cb14318572c8f654feeb3df
CharsonBurensen <1peruejos@Hdsb.ca>
2025-03-26 09:14:50 -0400

    Create disposables.js

---
+++ disposables.js
@@ -0,0 +1,14 @@
+elements.breakable_cloner = {
+    name: "BreakableCloner",
+    color: ["#2030b3"],
+    behavior: [
+    "XX|XX|XX",
+    "XX|XX|XX",
+    "XX|M1|XX",
+],
+    category: "machines",
+    state: "liquid",
+    temp: 20,
+    tempHigh: 1000,
+    stateHigh: "metal_scrap",
+};
