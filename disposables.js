bb85ca5b54f5175a1e44b0c317f61507c9fc5fdb
CharsonBurensen <1peruejos@Hdsb.ca>
2025-03-26 12:05:29 -0400

    Update disposables.js

--- disposables.js
+++ disposables.js
@@ -2,13 +2,35 @@ elements.breakable_cloner = {
     name: "BreakableCloner",
     color: ["#2030b3"],
     behavior: [
+    "XX|CF|XX",
+    "CF|XX|CF",
+    "XX|M1|XX",
+],
+    category: "machines",
+    state: "solid",
+    temp: 20,
+    tempHigh: 1000,
+    stateHigh: "metal_scrap",
+    breakInto: "metal_scrap",
+    hardness: 0.75,
+    burn: 0.1,
+};
+
+elements.shrapnel = {
+    name: "Shrapnel",
+    color: ["#2030b3"],
+    behavior: [
     "XX|XX|XX",
     "XX|XX|XX",
     "XX|M1|XX",
 ],
     category: "machines",
-    state: "liquid",
+    state: "solid",
     temp: 20,
     tempHigh: 1000,
     stateHigh: "metal_scrap",
+    breakInto: "metal_scrap",
+    hardness: 0.75,
+    burn: 0.1,
 };
+
